b48ddda81263cb98c7e0616ef0bbc2eb7c59b022
sai Hrithik <saihrithik31@gmail.com>
2024-05-23 15:19:53 -0400

    First commit

---
+++ BinarySearch.java
@@ -0,0 +1,69 @@
+class Node
+{
+    int data;
+    Node left;
+    Node right;
+
+    public Node(int data)
+    {
+        this.data = data;
+        left = null;
+        right = null;
+    }
+}
+
+public class BinarySearch {
+
+    Node root;
+
+    BinarySearch()
+    {
+        root = null;
+    }
+
+
+    public Node insert(int data)
+    {
+        if(root == null)
+        {
+            root = new Node(data);
+        }
+        else if(data < root.data)
+        {
+            root.left = insert(data); 
+        }
+        else if(data > root.data)
+        {
+            root.right = insert(data);
+        }
+
+        return root;
+    }
+
+    Node search(Node root, int data) {
+        // Base Cases: root is null or key is present at root
+        if (root == null || root.data == data)
+            return root;
+ 
+        // Key is greater than root's key
+        if (root.data < data)
+            return search(root.right, data);
+ 
+        // Key is smaller than root's key
+        return search(root.left, data);
+    }
+
+    public static void main(String args[])
+    {
+        BinarySearch key = new BinarySearch();
+        key.insert(8);
+        key.insert(12);
+        key.insert(10);
+        key.insert(17);
+
+        
+
+    }
+    
+}
+
